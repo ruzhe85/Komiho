4470c2cc823632537c9bb40b4ee61b6bc6567c30
ruzhe85 <ruzhe85@users.noreply.github.com>
2026-08-21 12:44:52 +0800

    fix(build): 补 IconButton 导入

--- app/src/main/java/app/mihonsy/komga/ui/KomgaMainActivity.kt
+++ app/src/main/java/app/mihonsy/komga/ui/KomgaMainActivity.kt
@@ -66,6 +66,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
